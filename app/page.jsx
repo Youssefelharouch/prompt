27172a5828b94fbd7f23ed3edbaabca1d471df35
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-12-18 17:56:20 +0100

    Update dependencies and remove unused files

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function page() {
+  return (
+    <section className="w-ful flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          AI TEST YOUR ABILITY
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores in
+        blanditiis nostrum quos id distinctio corporis assumenda ut quam porro,
+        accusantium, animi deleniti ratione. Voluptate aspernatur suscipit
+        eveniet quas cupiditate?
+      </p>
+    </section>
+  );
+}
+
+export default page;
