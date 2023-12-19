0c15b7bd9c66454337ea1a542e6560eab2d2d619
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-12-19 17:52:22 +0100

    Update .gitignore and add database connection in route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import nextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import { connectToDB} from "../../../utils/db";
 
 const handler = nextAuth({
   providers: [
@@ -12,9 +12,11 @@ const handler = nextAuth({
   async session(session) {},
   async signIn(profile) {
     try {
-      
+    await  connectToDB()
+    // check if a user already exists
+    // if not, create a new user  in the database
     } catch (error) {
-      
+      console.log(error);
     }
   },
 });
