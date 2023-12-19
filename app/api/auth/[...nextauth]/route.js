d3283ce427bbb8cb0afd233e259573ac977f3c13
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-12-19 18:03:22 +0100

    Fix user creation in session and sign-in handlers

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import nextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { connectToDB} from "../../../utils/db";
+import { connectToDB } from "../../../utils/db";
+import User from "../../../models/User";
 
 const handler = nextAuth({
   providers: [
@@ -9,12 +10,25 @@ const handler = nextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session(session) {},
-  async signIn(profile) {
+  async session({session}) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+  },
+
+  async signIn({profile}) {
     try {
-    await  connectToDB()
-    // check if a user already exists
-    // if not, create a new user  in the database
+      await connectToDB();
+      // check if a user already exists
+      const userExist = await User.findOne({ email: profile.email });
+      // if not, create a new user  in the database
+      if (!userExist) {
+        const newUser = new User({
+          userName: profile.name.replace(" ", "").toLowerCase(),
+          email: profile.email,
+          image: profile.picture,
+        });
+        await newUser.save();
+        return true;
+      }
     } catch (error) {
       console.log(error);
     }
